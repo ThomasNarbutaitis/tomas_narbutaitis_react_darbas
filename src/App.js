99108d886154b8ef68b0d0af22efe2e9e8fde728
Tomas <tomas_narba@yahoo.com>
2022-07-05 14:03:55 +0300

    pages routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Add from './pages/Add';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <h1>Boilerplate!</h1>
+      <Switch>
+        <Route path='/register'>
+          <Register />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/add'>
+          <Add />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
